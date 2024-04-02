3b1ff26668f9ee613decef69037b407f264f7c84
Odong Martin <odongmartin21@gmail.com>
2024-04-02 11:54:49 +0300

    sonner notifications

--- components/feedbackForm.tsx
+++ components/feedbackForm.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Toaster, toast } from 'sonner'
 
 interface FormData {
   name: string;
@@ -28,12 +29,14 @@ export default function FeedbackForm() {
       if (response.ok) {
         setFormData((prevFormData) => ({ ...prevFormData, message: '' }));
         // show success message to the user
-        alert('Feedback sent successfully');
+        //alert('Feedback sent successfully');
+        toast.success('Feedback sent successfully');
 
         console.log('Feedback sent successfully');
       } else {
+        toast.error('Failed to send feedback');
         console.error('Failed to send feedback');
-        alert('Failed to send feedback');
+        // alert('Failed to send feedback');
 
       }
     } catch (error) {
@@ -43,16 +46,17 @@ export default function FeedbackForm() {
 
   return (
     <>
-        <form className='grid gap-2 items-center justify-center text-black' onSubmit={handleSubmit}>
-          <span className='text-white mb-4 text-xl font-bold'>FeedBack</span>
-          <label className='text-white'>Name</label>
-          <input className='rounded-lg' type="text" name="name" value={formData.name} onChange={handleChange} />
-          <label className='text-white mt-4'>Email</label>
-          <input className='rounded-lg' type="email" name="email" value={formData.email} onChange={handleChange} />
-          <label className='text-white mt-4'>Message</label>
-          <textarea className='rounded-lg' name="message" value={formData.message} onChange={handleChange}></textarea>
-          <button type="submit" className='text-white border-2 rounded-lg w-fit p-1 flex items-center justify-center hover:border-green-500 hover:text-green-500 transition-colors ease-in-out duration-500'>Submit</button>
-        </form>
+      <Toaster richColors/>
+      <form className='grid gap-2 items-center justify-center text-black' onSubmit={handleSubmit}>
+        <span className='text-white mb-4 text-xl font-bold'>FeedBack</span>
+        <label className='text-white'>Name</label>
+        <input className='rounded-lg' type="text" name="name" value={formData.name} onChange={handleChange} />
+        <label className='text-white mt-4'>Email</label>
+        <input className='rounded-lg' type="email" name="email" value={formData.email} onChange={handleChange} />
+        <label className='text-white mt-4'>Message</label>
+        <textarea className='rounded-lg' required name="message" value={formData.message} onChange={handleChange}></textarea>
+        <button type="submit" className='text-white border-2 rounded-lg w-fit p-1 flex items-center justify-center hover:border-green-500 hover:text-green-500 transition-colors ease-in-out duration-500'>Submit</button>
+      </form>
     </>
   );
 }
